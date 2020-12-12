38257be4fc9915f3c1c0b19af6cb796d2ae22012
Tomas Gil <tomasgil1234@gmail.com>
2020-12-12 20:17:40 -0300

    feat: add list users to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,47 @@ import MainLayout from 'src/layouts/main'
 
 //types
 import PageWithLayout from 'src/types/pageWithLayout'
+import { User } from 'src/types/user'
 
-const Home: FC = () => {
-  return <div>Home</div>
+interface Users {
+  users: User[]
+}
+interface PropsPage {
+  props: Users
+}
+
+const Home: FC<Users> = ({ users }) => {
+  return (
+    <div>
+      Home
+      <div style={{ width: '300px', margin: 'auto' }}>
+        {users.map((user) => (
+          <div
+            style={{
+              padding: '10px',
+              border: '1px solid black',
+              margin: '5px',
+              textDecoration: user.id > 10 ? 'line-through' : 'inherit',
+            }}
+            key={user.id}
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 ;(Home as PageWithLayout).layout = MainLayout
+
+export async function getStaticProps(): Promise<PropsPage> {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const result = await response.json()
+  const users: User[] = result.map((user) => {
+    return { id: user.id, name: user.name }
+  })
+  return { props: { users } }
+}
 
 export default Home
