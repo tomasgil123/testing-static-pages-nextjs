a7832682c57908395f430574517700b653e219c6
Tomas Gil <tomasgil1234@gmail.com>
2020-12-13 16:19:57 -0300

    test: add tests for home page

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,7 @@
 import { rest } from 'msw'
 
+//we mock a situation where the first user has one video and the second user has two videos
+//the rest of the user do not have videos
 const mockVideos = [
   ['https://www.youtube.com/watch?v=91CzTz2-IJw'],
   ['https://www.youtube.com/watch?v=91CzTz2-IJw', 'https://www.youtube.com/watch?v=N6Kqg7bdKCQ'],
