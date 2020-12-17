b7f123cbd5d0c377a093b8013be433c28da5aeaf
Tomas Gil <tomasgil1234@gmail.com>
2020-12-17 07:38:50 -0300

    refactor: remove broswer part msw

--- mocks/index.ts
+++ mocks/index.ts
@@ -2,9 +2,6 @@
 if (typeof window === 'undefined') {
   const { server } = require('./server')
   server.listen()
-} else {
-  const { worker } = require('./browser')
-  worker.start()
 }
 
 export {}
